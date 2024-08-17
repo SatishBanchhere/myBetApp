c0055103b9542b72f6225e2ecb823b089131f201
Satish Banchhere <69459934+SatishBanchhere@users.noreply.github.com>
2024-08-17 12:13:23 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // src/config.js
 // const BASE_URL = "https://ca-backend24.onrender.com"; // Global base URL here
 // const BASE_URL = "http://localhost:8080"; // Local URL here
-const BASE_URL = "https://mybetappbackend-gi8c.onrender.com/user"; // Global 2nd base URL
+const BASE_URL = "https://mybetappbackend-gi8c.onrender.com"; // Global 2nd base URL
 
 export default {
   BASE_URL,
